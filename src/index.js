0662cc274e9f21ed0173420f8faa18ef9fa5016d
goodwyyn <godwin.mtuma@student.moringaschool.com>
2022-06-15 18:24:23 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault
+    handleToDo(e.target.new-task-description)
+  })
 });
+
+function handleToDo(todo){
+  let p = document.createElement('button')
+  Btn.textcontent = 'My Todos'
+  p.appendChild(btn)
+  document.querySelector('#My Todo_container').appendChild(p)
+}
